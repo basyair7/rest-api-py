37800a364eef8945ed79b125df418cd5b949a961
Fathul <basyair.fathul@gmail.com>
2024-01-09 21:30:31 +0700

    modified program

--- main/index.py
+++ main/index.py
@@ -1,9 +1,23 @@
-import requests, json
+import requests, json, os, keyboard
 from requests.auth import HTTPBasicAuth
+from dotenv import load_dotenv, dotenv_values
 
-res = requests.get('http://127.0.0.1:5000/api/v1/private', auth=HTTPBasicAuth('basyair', '1904105010004'));
-print(f'Response Code : {str(res.status_code)}');
-if(res.status_code == 200):
-    print(f'Login successful : {res.text}');
-    s = json.loads(res.text);
-    print(s["Maybe"]);
+load_dotenv()
+while True:
+    if keyboard.is_pressed('q'): break;
+    try:
+        print(f'{os.getenv("USERNAME")}, {os.getenv("PASSWORD")}')
+        res = requests.get(
+                'http://127.0.0.1:5000/api/v1/private', 
+                auth=HTTPBasicAuth(os.getenv("USERNAME"), os.getenv("PASSWORD"))
+            );
+
+        print(f'Response Code : {str(res.status_code)}');
+        if(res.status_code == 200):
+            print(f'Login successful : {res.text}');
+            s = json.loads(res.text);
+            print(s["Maybe"]);
+            
+    except:
+        print("error");
+        continue;
--- src/server.py
+++ src/server.py
@@ -1,27 +1,30 @@
 from flask import Flask
 from flask_restful import Resource, Api
 from flask_httpauth import HTTPBasicAuth
+from dotenv import load_dotenv, dotenv_values
+import os
 
 app = Flask(__name__);
 api = Api(app, prefix='/api/v1');
 auth = HTTPBasicAuth();
 
+load_dotenv();
 USER_DATA = {
-    "basyair": "1904105010004"
+    os.getenv("USERNAME") : os.getenv("PASSWORD")
 };
 
 @auth.verify_password
 def verify(username, password):
     if not (username and password):
-        return False
+        return False;
     return USER_DATA.get(username) == password;
 
 class PrivateResource(Resource):
     @auth.login_required
     def get(self):
-        return {"Maybe": 24};
+        return {"Maybe": 30};
     
 api.add_resource(PrivateResource, '/private');
 
 if(__name__ == '__main__'):
-    app.run(debug=False);
+    app.run(debug=True);
